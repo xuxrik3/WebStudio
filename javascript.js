b43e9dd3d0ae37a1ffe0bc7719043f48f9a53714
xuxrik3 <140961972+xuxrik3@users.noreply.github.com>
2025-03-17 23:24:04 +0100

    е
    е

--- javascript.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("instagram1").addEventListener("mouseover", function() {
-//     this.src = "./img/instagram1.svg";
-// });
-// document.getElementById("instagram1").addEventListener("mouseout", function() {
-//     this.src = "./img/instagram2.svg";
-// });
